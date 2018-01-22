a80537150e997261d226b4491b03b63efbb454c6
Git-leng <leng_bw@163.com>
2018-01-22 10:18:13 +0800

    格式化一下

--- template/common/util.js
+++ template/common/util.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 const instance = axios.create();
 
 function createAPI(baseURL) {
-	return function(conf) {
-		conf = conf || {};
-		return instance(Object.assign({}, {
-			url: conf.url,
-			baseURL: baseURL,
-			method: conf.method
-		}, conf.opts));
-	};
+    return function (conf) {
+        conf = conf || {};
+        return instance(Object.assign({}, {
+            url: conf.url,
+            baseURL: baseURL,
+            method: conf.method
+        }, conf.opts));
+    };
 }
 
 export {
-	createAPI
+    createAPI
 };
--- template/init/instance.js
+++ template/init/instance.js
@@ -1,9 +1,9 @@
 import { createAPI } from '<%- $$.relative("util") %>';
 
 const baseUrl = {
-	development: '',
-	prepub: '',
-	production: ''
+    development: '',
+    prepub: '',
+    production: ''
 }[process.env.NODE_ENV];
 
 export default createAPI(baseUrl);
