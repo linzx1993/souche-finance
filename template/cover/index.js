dca74f92e7ccd7d1ba0d9465a3acbaa6f1ff2a55
admin <admin@admindeMacBook-Air-4.local>
2018-11-23 14:49:58 +0800

    feat: vue3.x适用的数据模板

--- template/cover/index.js
+++ template/cover/index.js
@@ -1,8 +1,11 @@
 /* eslint-disable */
-import instance from './instance';
+import Util from '../util';
 
-<% _.forEach(data, function(o){ %> /** <%- o.summary %> */
-function <%- $$.convertUrl(o.path) %> (opts) {
+let domain = process.env.<%- data.project.domain || 'DOMAIN' %>;
+let instance = Util.finance(domain);
+<% _.forEach(data.list, function(o){ %>
+/** <%- o.summary %> */
+export function <%- $$.convertUrl(o.path) %> (opts) {
     return instance({
         method: '<%- o.method %>',
         url: '<%- o.path %>',
@@ -10,9 +13,4 @@ function <%- $$.convertUrl(o.path) %> (opts) {
         res: <%- o.responseMessages[0].responseModel %>
     });
 }
-
 <% }) %>
-export {
-    <% _.forEach(data, function(o, i){ %><%- $$.convertUrl(o.path) %><% if(data.length - 1 !== i) { %>,
-    <% } %><% }) %>
-};
