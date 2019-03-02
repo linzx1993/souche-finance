5778bfb46cd093cd0c4443bd62df0efa42cf9c74
冷博文 <leng_bw@163.com>
2019-03-02 16:17:12 +0800

    feat: react-native接口请求模板文件创建

--- template/common/util.js
+++ template/common/util.js
@@ -1,3 +1,11 @@
+/**
+ * @file util.js
+ * @desc react-native接口请求工具类封装
+ * @author leng_bw@163.com
+ * @version 1.0.0
+ * @date 2019-03-02
+ */
+
 import { SRNStore } from '@souche-f2e/srn-framework';
 
 const instance = new SRNStore().__fetch;
@@ -14,6 +22,6 @@ function createAPI(baseURL) {
     };
 }
 
-export {
+export default {
     createAPI
 };
--- template/init/instance.js
+++ template/init/instance.js
@@ -1,3 +1,11 @@
+/**
+ * @file instance.js
+ * @desc react-native接口host配置
+ * @author leng_bw@163.com
+ * @version 1.0.0
+ * @date 2019-03-02
+ */
+
 import { SRNConfig } from '@souche-f2e/srn-framework';
 import Util from '../util.js';
 
